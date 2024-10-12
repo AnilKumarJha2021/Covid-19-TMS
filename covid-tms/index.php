8ddf8043bcfc62830fd37523f408b762409ef562
AnilKumarJha2021 <kumaranilraj.2802@gmail.com>
2024-10-12 12:28:44 +0530

    Php and Sql project

---
+++ covid-tms/index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Covid-19 Testing Management System</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/scrolling-nav.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">Covid19-TMS</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">About Coronavirus </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#services">Covid-19 Symptoms</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#contact">Prevention</a>
+          </li>
+  <li class="nav-item active">
+            <a class="nav-link js-scroll-trigger" href="new-user-testing.php">Testing</a>
+          </li>
+           <li class="nav-item active">
+            <a class="nav-link js-scroll-trigger" href="live-test-updates.php">Live Updates</a>
+          </li>
+
+           <li class="nav-item active">
+            <a class="nav-link js-scroll-trigger" href="login.php">Admin</a>
+          </li>
+
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <header class="bg-primary text-white">
+    <div class="container text-center">
+      <h1>COVID19-TMS</h1>
+      <p class="lead">COVID19 - Testing Management System</p>
+    </div>
+  </header>
+
+  <section id="about">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2><b>About this page</b></h2>
+          <p class="lead" style="text-align:justify;">COVID-19 is an infectious disease caused by the coronavirus SARS-CoV-2, first identified in December 2019 in Wuhan, China. 
+            It spread rapidly worldwide, leading to the COVID-19 pandemic. The virus primarily spreads through respiratory droplets, and its symptoms 
+            can range from mild (fever, cough, fatigue) to severe (difficulty breathing, pneumonia, multi-organ failure). Asymptomatic cases are also 
+            common..</p><br><br><br>
+            <h2><b>Key Phases of the Pandemic:</b></h2>
+          <p class="lead" style="text-align:justify;">The COVID-19 virus spread primarily through droplet of saliva or discharge from the nose when an 
+          infected person coughs or sneezes so it’s important that you also practice respiratory etiquette.
+          <ol type="1" >
+          <li style="text-align:justify;"> Outbreak & Initial Spread (Late 2019 - Early 2020):</li>     
+          <ul type="circle">
+          <li style="text-align:justify;">The virus was first identified in December 2019.</li>
+          <li style="text-align:justify;">It rapidly spread to other countries, leading to global outbreaks.</li> 
+          </ul>  
+          <li style="text-align:justify;">Pandemic Declaration (March 2020):  </li> 
+          <ul type="circle">
+          <li style="text-align:justify;">The World Health Organization (WHO) declared COVID-19 a pandemic in March 2020.</li>
+          <li style="text-align:justify;">Countries imposed lockdowns, travel restrictions, and other public health measures.</li> 
+          </ul> 
+          <li style="text-align:justify;">Vaccine Development (2020-2021):  </li> 
+          <ul type="circle">
+          <li style="text-align:justify;">Multiple vaccines were developed in record time using different technologies (mRNA, viral vectors, protein subunits).</li>
+          <li style="text-align:justify;">Mass vaccination campaigns began globally, helping reduce the severity and spread of the virus.</li> 
+          </ul> 
+          <li style="text-align:justify;">Variants & Ongoing Challenges (2021-2023):  </li> 
+          <ul type="circle">
+          <li style="text-align:justify;">New variants (Alpha, Delta, Omicron, etc.) emerged, with varying transmissibility and resistance to vaccines.</li>
+          <li style="text-align:justify;">Efforts focused on booster shots, ongoing public health measures, and adapting vaccines.</li> 
+          </ul>            
+          </ol>
+        </p>
+         
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="services" class="bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>Covid-19 Symptoms</h2>
+          <hr />
+<p><strong>Hight Fever 2-14 days!</strong><br />
+Reported illnesses have ranged from mild symptoms to severe illness and death</p>
+             <hr />
+<p><strong>Dry Cough 2-14 days!</strong><br />
+Reported illnesses have ranged from mild symptoms to severe illness and death</p>
+          <hr />
+<p><strong>Shortness of breath!</strong><br />
+Reported illnesses have ranged from mild symptoms to severe illness and death</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="contact">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2>Prevention</h2>
+  <ul>
+            <li>Wash your Hands often</li>
+            <li>Wear A Face mask</li>
+            <li>Avoid contact with sick people</li>
+            <li>Always cover your cough or sneeze</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; ANIL & JAI</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom JavaScript for this theme -->
+  <script src="js/scrolling-nav.js"></script>
+
+</body>
+
+</html>
